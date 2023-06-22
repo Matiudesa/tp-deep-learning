ed4a1c217c58532d1178c763faa102f6e647fec4
lucas-emartinez <88638100+lucas-emartinez@users.noreply.github.com>
2023-06-22 02:37:54 -0300

    Clase Personas finalizada
    Se debe verificar el metodo remove_from_df para ver si funciona
    
    Falta terminar la clase Trabajadores haciendo un override de get_stats

--- controllers/personas.py
+++ controllers/personas.py
@@ -1,11 +1,13 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 import pandas as pd
 from datetime import datetime
 from matplotlib import pyplot as plt
 
+PERSONAS_CSV_ROUTE = 'data/personas.csv'
 
-class Personas(ABC):
+class Personas():
 
+    # Constructo definiendo los atributos
     def __init__(self, nombre, fecha_nacimiento, genero, codigo_postal, id = None):
         self.nombre = nombre
         self.fecha_nacimiento = fecha_nacimiento
@@ -13,6 +15,8 @@ class Personas(ABC):
         self.codigo_postal = codigo_postal
         self.id = id
 
+
+    # Dunder para imprimir atributos del objeto instanciado
     def __repr__(self):
         string = list()
         string.append(f'ID: {self.id}')
@@ -21,11 +25,11 @@ class Personas(ABC):
         string.append(f'Codigo postal: {self.codigo_postal}')
         return "\n".join(string)
 
+    #Metodo que retorna un dataframe a partir del archivo csv en la ruta pasada por parametro filename
     @classmethod
     def create_df_from_csv(cls, filename):
-        df_mov = pd.read_csv(filename)
-
-        return df_mov
+        df_personas = pd.read_csv(filename)
+        return df_personas
 
     @classmethod
     def get_from_df(cls, df_mov, id=None, nombre=None, anios=None, generos=None):
@@ -50,12 +54,29 @@ class Personas(ABC):
 
         return result_df
 
-    # @abstractmethod
-    # def write_df(self):
-    #  pass
+
+    # Metodo  debe ser sobreescrito en las clases hijas, ya que crearemos un metodo 
+    # para cada usuario o trabajadores que hereden de esta clase.
+    def write_df(self, df_personas, overwrite=False):
+        
+        if (self.id != None):
+            
+
+            if overwrite:
+                df_personas.loc[df_personas['id'] == self.id] = [self.id, self.nombre, self.fecha_nacimiento, self.genero, self.codigo_postal]
+                df_personas.to_csv(PERSONAS_CSV_ROUTE, index=False)
+                print("Se ha actualizado el registro personas.")
+            else:
+                  raise ValueError(f'El ID {self.id} ya existe en el archivo.')
+        else:
+            self.id = df_personas['id'].max() + 1
+            df_personas.loc[df_personas.shape[0]] = [self.id, self.nombre, self.fecha_nacimiento, self.genero, self.codigo_postal]
+            df_personas.to_csv(PERSONAS_CSV_ROUTE, index=False)
+            print("Se ha creado el registro en personas.")
+
 
     @classmethod
-    def get_stats(cls, df_mov, anios=None, generos=None):
+    def get_stats(cls, df_personas, anios=None, generos=None):
         query = []
 
         if(anios != None):
@@ -67,25 +88,61 @@ class Personas(ABC):
         
         if len(query) > 0:
             query_str = ' and '.join(query)
-            result_df = df_mov.query(query_str)
+            result_df = df_personas.query(query_str)
         else:
-            result_df = df_mov
+            result_df = df_personas
 
         if result_df.empty:
             print("No se encontraron usuarios que cumplan los requisitos.")
             return
 
-        #Cantidad de tuplas (personas)
-        print(result_df.shape[0])
-        #Cantidad
+        #Cantidad total de tuplas segun query (personas)
+        print(f'Cantidad total de personas segun busqueda: {result_df.shape[0]}')
+       
 
-    # @abstractmethod
-    def remove_from_df(self, df_mov):
-        pass
+        # Crea un histograma para cada género de la cantidad nacida de hombres y mujeres por año
+        #  siempre y cuando lo años y generos no se especifiquen
+        if anios == None and generos == None:
+             # Creacion de dataframe para cada género
+            df_hombres = result_df[result_df['Gender'] == 'M']
+            df_mujeres = result_df[result_df['Gender'] == 'F']
+            plt.hist(df_hombres['year of birth'], bins=range(result_df['year of birth'].min(), result_df['year of birth'].max()+1), alpha=0.5, label='Hombres', color='blue', edgecolor='black')
+            plt.hist(df_mujeres['year of birth'], bins=range(result_df['year of birth'].min(), result_df['year of birth'].max()+1), alpha=0.5, label='Mujeres', color='pink', edgecolor='black')
+
+            plt.title('Distribución de años de nacimiento por género')
+            plt.xlabel('Año de nacimiento')
+            plt.ylabel('Cantidad de personas')
+            plt.title('Distibución de años de nacimiento por género')
+            plt.title(f'''Distibución de años de nacimiento por género
+                      Total: {result_df.shape[0]}, Hombres: {df_hombres.shape[0]}, Mujeres: {df_mujeres.shape[0]}''')
+            # Añade leyenda para indicar qué color corresponde a cada género
+            plt.legend(loc='upper right')
+            plt.show()
+
+    # Metodo que elimina un registro de personas en el archivo csv solamente cuando todos los atributos del objeto instanciado 
+    # coinciden con los del registro   
+    def remove_from_df(self, df):
+       
+        match = ( 
+                  (df['id'] == self.id) &
+                  (df['Full Name'] == self.nombre) &
+                  (df['year of birth'] == self.fecha_nacimiento) &
+                  (df['Gender'] == self.genero) &
+                  (df['Zip Code'] == self.codigo_postal)
+                )
+       
+        if match.any():
+            df = df.drop(match.index)
+            df.to_csv(PERSONAS_CSV_ROUTE, index=False)
+            print("Se ha eliminado el registro de personas.")
+        else:
+            print("No se ha encontrado el registro de personas.")
+           
 
     
+# Metodo que retorna un dataframe a partir del archivo csv en la ruta pasada por parametro
+df_personas = Personas.create_df_from_csv(PERSONAS_CSV_ROUTE)
 
 
-filename = 'data/personas.csv'
-df_personas = Personas.create_df_from_csv(filename)
-Personas.get_stats(df_personas, anios=45)
+
+
--- controllers/trabajadores.py
+++ controllers/trabajadores.py
@@ -1,4 +1,8 @@
-from personas import Personas
+from personas import Personas, PERSONAS_CSV_ROUTE
+import pandas as pd
+
+TRABAJADORES_CSV_ROUTE = 'data/trabajadores.csv'
+
 class Trabajadores(Personas):
 
     def __init__(self, nombre, fecha_nacimiento, genero, codigo_postal, fecha_alta, puesto, horario_trabajo, categoria, id = None):
@@ -16,17 +20,43 @@ class Trabajadores(Personas):
         trabajadores_repr.append(f'Horario de trabajo: {self.horario_trabajo}')
         trabajadores_repr.append(f'Categoria: {self.categoria}')
         return base_repr + "\n" + "\n".join(trabajadores_repr)
+    
+    @classmethod
+    def create_df_from_csv(cls, filename):
+        df_trabajadores = pd.read_csv(filename)
+        return df_trabajadores
 
 
+    def write_df(self, df_trabajadores, overwrite = False):  
+        
+        if self.id is not None:
+            if overwrite:
+                df_trabajadores.loc[df_trabajadores['id'] == self.id] = [ self.id, self.puesto, self.categoria, self.horario_trabajo, self.fecha_alta]
+                df_trabajadores.to_csv(TRABAJADORES_CSV_ROUTE, index=False)
+                print("Se ha actualizado el registro en trabajadores.")
+            else:
+                 raise ValueError(f'El ID {self.id} ya existe en el archivo.')
+        else:
+            df_personas = super().create_df_from_csv(PERSONAS_CSV_ROUTE)    
+            super().write_df(df_personas)
+            self.id = df_personas.shape[0]
+            df_trabajadores.loc[df_trabajadores.shape[0]] = [self.id, self.puesto, self.categoria, self.horario_trabajo, self.fecha_alta]
+            df_trabajadores.to_csv(TRABAJADORES_CSV_ROUTE, index=False)
+            print("Se ha creado el registro en trabajadores.")
+            
+
+
+# Objeto instanciado para probar el funcionamiento de la clase
 t = Trabajadores(
     fecha_alta= '15/05/2005',
-    fecha_nacimiento= '11/04/2000',
+    fecha_nacimiento= '2000',
     nombre = 'Lucas Martinez',
-    genero='H',
-    categoria='C',
-    puesto='Ingeniero',
-    horario_trabajo='09.00-18.00hs',
-    id=1254,
+    genero='M',
+    categoria='A',
+    puesto='IT',
+    horario_trabajo='9-18',
     codigo_postal='B1713'
 )
-print(t)
+
+# Metodo que retorna un dataframe a partir del archivo csv en la ruta pasada por parametro
+df_trabajadores = Trabajadores.create_df_from_csv(TRABAJADORES_CSV_ROUTE)
