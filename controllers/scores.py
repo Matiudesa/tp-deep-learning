ffe56e821e8c387a4d873bca3a917c256d378292
lucas-emartinez <88638100+lucas-emartinez@users.noreply.github.com>
2023-06-28 23:39:15 -0300

    Scores implementado

--- controllers/scores.py
+++ controllers/scores.py
@@ -27,6 +27,25 @@ class Scores:
         df_scores["Date"] = df_scores["Date"].fillna('1900-01-01')
         df_scores["Date"] = df_scores["Date"].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
         return df_scores
+    
+    @classmethod
+    def get_from_df(cls, df_scores, usuario_id=None, pelicula_id=None, puntuacion=None) -> pd.DataFrame:
+        query = []
+
+        if usuario_id is not None:
+            query.append(f' user_id == {usuario_id} ')
+        if pelicula_id is not None:
+            query.append(f'movie_id == {pelicula_id}')
+        if puntuacion is not None:
+            query.append(f' rating == {puntuacion}')
+
+        if len(query) > 0:
+            query_str = " and ".join(query)
+            result_df = df_scores.query(query_str)
+        else:
+            result_df = df_scores
+
+        return result_df
 
     def write_df(self, df_scores, overwrite=False) -> None:
 
@@ -40,19 +59,43 @@ class Scores:
         if df_peliculas.loc[df_peliculas['id'] == self.pelicula_id].empty:
             print("El ID de película no existe.")
             return
+        
+        # el Timestamp no debe tener milisegundos
+
+        self.timestamp = self.timestamp.replace(microsecond=0)
+
 
         # Si los IDs de usuario y película existen, proceder como antes
         if overwrite:
-            df_scores.loc[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)] = [self.usuario_id, self.pelicula_id, self.puntuacion, self.timestamp]
+            row_index = df_scores.loc[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)].index[0]
+            df_scores.loc[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)] = [row_index, self.usuario_id, self.pelicula_id, self.puntuacion, self.timestamp]
             df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
+            print(f'Se ha actualizado la puntuacion de la pelicula {df_peliculas.loc[df_peliculas["id"] == self.pelicula_id]["Name"].values[0]}')
         else:
-            df_scores.loc[df_scores.shape[0]] = [self.usuario_id, self.pelicula_id, self.puntuacion, self.timestamp]
+            df_scores.loc[df_scores.shape[0]] = [df_scores.shape[0], self.usuario_id, self.pelicula_id, self.puntuacion, self.timestamp]
             df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
-            print("Se ha creado el registro en trabajadores.")
+            print(f'{self.puntuacion} puntos para la película {df_peliculas.loc[df_peliculas["id"] == self.pelicula_id]["Name"].values[0]}')
 
     def remove_from_df(self, df_scores) -> None:
-        df_scores = df_scores.drop(df_scores[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)].index)
-        df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
+
+        # matcheamos todas las propiedades del objeto
+        # No tiene sentido pasar un timestamp ya que toma el horario actual, eliminaremos solo por usuario y película y puntuacion
+        match = (
+                (df_scores['user_id'] == self.usuario_id) & 
+                (df_scores['movie_id'] == self.pelicula_id) &
+                (df_scores['rating'] == self.puntuacion)
+        )
+        
+        if match.any():
+            row_index = df_scores.loc[match].index[0]
+            df_scores = df_scores.drop(row_index)
+            df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
+            print(f'Se ha eliminado la puntuacion de la pelicula {self.pelicula_id}')
+        else:
+            print(f'No se ha encontrado puntuacion a eliminar')
+
+        # df_scores = df_scores.drop(df_scores[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)].index)
+        # df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
 
     def get_user_avg(self, df_scores):
         return df_scores.groupby('user_id')['rating'].mean()
@@ -66,18 +109,17 @@ class Scores:
 
     def get_movie_avg_by_id(self, df_scores, movie_id):
         movie_ratings = df_scores[df_scores['movie_id'] == movie_id]['rating']
-        return movie_ratings.mean()
+        return int(movie_ratings.mean())
 
 
 df_scores = Scores.create_df_from_csv(SCORES_CSV_ROUTE)
 
 score = Scores(
-    usuario_id=1,
-    pelicula_id=1,
-    puntuacion=5,
+    usuario_id=12,
+    pelicula_id=45,
+    puntuacion=4,
     timestamp=datetime.now()
-)
-
+    )
 '''
 user_avg = score.get_user_avg(df_scores)
 print("Calificación promedio por usuario:")
@@ -87,12 +129,17 @@ movie_avg = score.get_movie_avg(df_scores)
 print("Calificación promedio por película:")
 print(movie_avg)
 '''
-id = 5
-movie_avg_by_id = score.get_movie_avg_by_id(df_scores, id)
-print(f"Calificación promedio de la película con ID {id}:")
-print(movie_avg_by_id)
+# id = 5
+# movie_avg_by_id = score.get_movie_avg_by_id(df_scores, id)
+# print(f"Calificación promedio de la película con ID {id}:")
+# print(movie_avg_by_id)
 
 
+# Obtener del dataframe
+print(Scores.get_from_df(df_scores, usuario_id=2))
+
+# Eliminar mediante matcheos
+# score.remove_from_df(df_scores)
 
 
 
