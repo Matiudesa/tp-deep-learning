19038120e82554bfa0c8c37c30e98b81934d2e75
MEVG <matiasevange@gmail.com>
2023-06-28 22:10:30 -0300

    Stats Scores

--- controllers/scores.py
+++ controllers/scores.py
@@ -20,14 +20,14 @@ class Scores:
         string.append(f'Puntuacion: {self.puntuacion}')
         string.append(f'Timestamp: {self.timestamp}')
         return "\n".join(string)
-    
+
     @classmethod
     def create_df_from_csv(cls, filename) -> pd.DataFrame:
         df_scores = pd.read_csv(filename)
         df_scores["Date"] = df_scores["Date"].fillna('1900-01-01')
         df_scores["Date"] = df_scores["Date"].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
         return df_scores
-    
+
     def write_df(self, df_scores, overwrite=False) -> None:
 
         df_peliculas = pd.read_csv(PELICULAS_CSV_ROUTE)
@@ -53,7 +53,21 @@ class Scores:
     def remove_from_df(self, df_scores) -> None:
         df_scores = df_scores.drop(df_scores[(df_scores['user_id'] == self.usuario_id) & (df_scores['movie_id'] == self.pelicula_id)].index)
         df_scores.to_csv(SCORES_CSV_ROUTE, index=False)
-        
+
+    def get_user_avg(self, df_scores):
+        return df_scores.groupby('user_id')['rating'].mean()
+
+    def get_movie_avg(self, df_scores):
+        return df_scores.groupby('movie_id')['rating'].mean()
+
+    def get_user_avg_by_id(self, df_scores, user_id):
+        user_ratings = df_scores[df_scores['user_id'] == user_id]['rating']
+        return user_ratings.mean()
+
+    def get_movie_avg_by_id(self, df_scores, movie_id):
+        movie_ratings = df_scores[df_scores['movie_id'] == movie_id]['rating']
+        return movie_ratings.mean()
+
 
 df_scores = Scores.create_df_from_csv(SCORES_CSV_ROUTE)
 
@@ -64,10 +78,21 @@ score = Scores(
     timestamp=datetime.now()
 )
 
-score.write_df(df_scores, overwrite=True)
+'''
+user_avg = score.get_user_avg(df_scores)
+print("Calificación promedio por usuario:")
+print(user_avg)
+
+movie_avg = score.get_movie_avg(df_scores)
+print("Calificación promedio por película:")
+print(movie_avg)
+'''
+id = 5
+movie_avg_by_id = score.get_movie_avg_by_id(df_scores, id)
+print(f"Calificación promedio de la película con ID {id}:")
+print(movie_avg_by_id)
 
 
 
 
-    
-        
+
