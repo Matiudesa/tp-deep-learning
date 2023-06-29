064b749980557ae8fd61e364f8b160e2e2ad38ec
MEVG <matiasevange@gmail.com>
2023-06-29 00:11:38 -0300

    Save backup

---
+++ controllers/save.py
@@ -0,0 +1,15 @@
+from peliculas import PELICULAS_CSV_ROUTE
+from personas import PERSONAS_CSV_ROUTE
+from scores import SCORES_CSV_ROUTE
+from trabajadores import TRABAJADORES_CSV_ROUTE
+import shutil
+
+def save_all():
+    shutil.copyfile(PELICULAS_CSV_ROUTE, 'backup/peliculas.csv')
+    shutil.copyfile(PERSONAS_CSV_ROUTE, 'backup/personas.csv')
+    shutil.copyfile(SCORES_CSV_ROUTE, 'backup/scores.csv')
+    shutil.copyfile(TRABAJADORES_CSV_ROUTE, 'backup/trabajadores.csv')
+    print("Se han guardado todos los archivos CSV.")
+
+
+save_all()
